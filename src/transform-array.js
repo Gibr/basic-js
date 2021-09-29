ff10ad4fab55e201b7446c9fcdb294943bfad2d8
Denis-Politsarnov <todayz@yandex.ru>
2021-09-30 02:31:59 +0300

    transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,36 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  const newArr = [];
+  const discarded = {};
+  
+  arr.forEach((value, index) => {
+    if (typeof value === "string" && value.startsWith("--")) {
+      switch (value) {
+        case "--discard-next":
+          discarded[index+1] = true;
+        break;
+        case "--discard-prev":
+          if (newArr[newArr.length - 1] && !discarded[index-1]) {
+            discarded[index-1] = true;
+            newArr.pop();
+          } 
+        break;
+        case "--double-next":
+          if (arr[index + 1]) newArr.push(arr[index + 1]);
+        break;
+        case "--double-prev":
+          if (newArr[newArr.length - 1] && !discarded[index-1]) newArr.push(newArr[newArr.length - 1]);
+        break;
+        default:
+          if (!discarded[index]) newArr.push(value);
+        break;
+      }
+    } else if (!discarded[index]) {
+      newArr.push(value);
+    }
+  });  
+  return newArr
 }
